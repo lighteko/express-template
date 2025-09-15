7519e737c5876532e05620a987b9652ba5d41409
lighteko <dev.lighteko@gmail.com>
2025-09-15 16:19:33 +0900

    [init] initial settings

---
+++ lib/config.ts
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import { Express } from "express";
+import * as process from "node:process";
+
+dotenv.config();
+
+export class BaseConfig {
+    static LOGGING_PATH = "../logs";
+    static PG_HOST = process.env.PG_HOST || "";
+    static PG_PORT = process.env.PG_PORT || "";
+    static PG_USER = process.env.PG_USER || "";
+    static PG_PASSWORD = process.env.PG_PASSWORD || "";
+    static PG_DB = process.env.PG_DB || "";
+    static PG_POOL_SIZE = process.env.PG_POOL_SIZE || "";
+    static PG_SCHEMA = process.env.PG_SCHEMA || "";
+
+    constructor(app: Express) {
+        BaseConfig.initApp(app);
+    }
+
+    static initApp(app: Express): void {
+        app.set("config", {
+            PG_HOST: this.PG_HOST,
+            PG_PORT: this.PG_PORT,
+            PG_USER: this.PG_USER,
+            PG_PASSWORD: this.PG_PASSWORD,
+            PG_DB: this.PG_DB,
+            PG_POOL_SIZE: this.PG_POOL_SIZE,
+            PG_SCHEMA: this.PG_SCHEMA,
+        });
+
+        console.log("App configuration initialized.");
+    }
+}
---
+++ src/output.ts
@@ -0,0 +1,31 @@
+import { Response } from "express";
+
+export function send(res: Response, code: number, data: object) {
+  return res.status(code).json({ data });
+}
+
+export function sendTokens(
+  res: Response,
+  tokens: { accessToken: string; refreshToken: string },
+  payload: any = {}
+) {
+  return res.status(200).json({
+    data: {
+      ...payload,
+      accessToken: tokens.accessToken,
+      refreshToken: tokens.refreshToken,
+    },
+  });
+}
+
+export function clearTokens(res: Response, message = "Logout succeeded") {
+  return res.status(200).json({
+    data: { message },
+  });
+}
+
+export function abort(res: Response, code: number, description: string) {
+  return res.status(code).json({
+    data: { message: description },
+  });
+}
